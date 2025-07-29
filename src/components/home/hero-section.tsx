c4540de7b0f6ce3faf7d7a22557dfbec3fc9d2c3
Navdeep <navdeep.code@gmail.com>
2025-07-29 18:04:36 +0000

    Still not working , actually write the code like it will eliminate this

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -15,49 +15,36 @@ export function HeroSection() {
         Autoplay({ delay: 4000, stopOnInteraction: true })
     );
     return (
-        <section className="relative w-full bg-background">
-            <div className="hidden md:block">
-                <Carousel
-                    plugins={[plugin.current]}
-                    className="w-full"
-                    onMouseEnter={plugin.current.stop}
-                    onMouseLeave={plugin.current.reset}
-                    opts={{
-                        loop: true,
-                    }}
-                >
-                    <CarouselContent>
-                        {mainCarouselImages.map((image, index) => (
-                            <CarouselItem key={index}>
-                                <div className="relative h-[95vh] w-full">
-                                    <Image
-                                        src={image.src}
-                                        alt={image.alt}
-                                        fill
-                                        className="object-cover object-center"
-                                        priority={index === 0}
-                                        sizes="100vw"
-                                    />
-                                    <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/20 to-transparent" />
-                                </div>
-                            </CarouselItem>
-                        ))}
-                    </CarouselContent>
-                    <CarouselPrevious className="absolute left-4 top-1/2 -translate-y-1/2 z-30 h-10 w-10 sm:h-12 sm:w-12 bg-white/20 hover:bg-white/40 text-white border-none" />
-                    <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2 z-30 h-10 w-10 sm:h-12 sm:w-12 bg-white/20 hover:bg-white/40 text-white border-none" />
-                </Carousel>
-            </div>
-            <div className="md:hidden relative h-[70vh] w-full">
-                <Image
-                    src={mainCarouselImages[0].src}
-                    alt={mainCarouselImages[0].alt}
-                    fill
-                    className="object-cover object-center"
-                    priority
-                    sizes="100vw"
-                />
-                <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/20 to-transparent" />
-            </div>
+        <section className="relative w-full bg-background hidden md:block">
+            <Carousel
+                plugins={[plugin.current]}
+                className="w-full"
+                onMouseEnter={plugin.current.stop}
+                onMouseLeave={plugin.current.reset}
+                opts={{
+                    loop: true,
+                }}
+            >
+                <CarouselContent>
+                    {mainCarouselImages.map((image, index) => (
+                        <CarouselItem key={index}>
+                            <div className="relative h-[95vh] w-full">
+                                <Image
+                                    src={image.src}
+                                    alt={image.alt}
+                                    fill
+                                    className="object-cover object-center"
+                                    priority={index === 0}
+                                    sizes="100vw"
+                                />
+                                <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/20 to-transparent" />
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious className="absolute left-4 top-1/2 -translate-y-1/2 z-30 h-10 w-10 sm:h-12 sm:w-12 bg-white/20 hover:bg-white/40 text-white border-none" />
+                <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2 z-30 h-10 w-10 sm:h-12 sm:w-12 bg-white/20 hover:bg-white/40 text-white border-none" />
+            </Carousel>
         </section>
     );
 }
